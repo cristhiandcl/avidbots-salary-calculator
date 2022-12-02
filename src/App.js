214959db86f1970088fe5ff8fecf5aad2572a18a
Cristhian Carrillo <carrillocristhian2408@gmail.com>
2022-12-01 20:10:30 -0500

    new files added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          React Js
-        </p>
-      </header>
+      <p className="text-green-300">Avidbots calculator</p>
     </div>
   );
 }
